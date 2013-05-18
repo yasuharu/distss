b0117d0ed9b4b7f81041300e9e2792cc2839dc08
Yasuharu Shibata <admin@mail.yasuharu.net>
2013-05-18 16:19:33 +0900

    * batchテストにログ表示を追加

--- test/batch_test.sh
+++ test/batch_test.sh
@@ -1,25 +1,33 @@
+#! /bin/bash
 
 TARGET_PATH="./test/batch"
 RUBY="ruby"
 
+
 if [ $# -eq 1 ]; then
 	RUBY=$1
 fi
-echo $RUBY
+echo "using $RUBY"
 
 for file in `ls $TARGET_PATH`
 do
+	echo "----- load $TARGET_PATH/$file -----"
+	echo "start server."
+	echo "start executer."
 	$RUBY server.rb --start
 	$RUBY executer.rb --start
 
 	# 異常終了したら終わる
+	echo "runngin client."
 	$RUBY client.rb -c $TARGET_PATH/$file
 	if [ $? -ne 0 ]; then
+		echo "failed test."
 		$RUBY executer.rb --stop
 		$RUBY server.rb --stop
 		exit 1
 	fi
 
+	echo "success test."
 	$RUBY executer.rb --stop
 	$RUBY server.rb --stop
 done
